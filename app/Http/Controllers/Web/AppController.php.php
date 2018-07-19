813315382e48501cea5e37ab75b5ea5af1366384
Joseph Matembu <jmatembu@gmail.com>
2018-07-19 15:49:53 +0300

    Initial commit

---
+++ app/Http/Controllers/Web/AppController.php.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+//use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AppController.php extends Controller
+{
+	public function app()
+	{
+		return view('app');
+	} 
+}
---
+++ resources/assets/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Vue Router Demo App</h1>
+
+        <nav>
+            <router-link :to="{ name: 'home' }">Home</router-link>
+            <router-link :to="{ name: 'hello' }">Hello World</router-link>
+            <router-link :to="{ name: 'users.index' }">Users</router-link>
+        </nav>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
